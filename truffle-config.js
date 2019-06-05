9ca10b8cd9d99c7594b4f0c35363e45beef6796f
Singh <nishant.f.singh@capgemini.com>
2019-06-05 16:26:43 +0530

    election

---
+++ truffle-config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
+  //change the setting 7545
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: "*" // Match any network id
+    }
+  },
+  compilers: {
+    solc: {
+      version: '0.4.25',
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  }
+};
